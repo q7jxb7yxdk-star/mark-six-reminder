1c2a71a6886db1faf5aa0d70567c1129ecaf6091
q7jxb7yxdk-star <q7jxb7yxdk@privaterelay.appleid.com>
2026-07-15 20:35:55 +0800

    Initial Commit

---
+++ Mark Six Reminder/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Mark Six Reminder
+//
+//  Created by Sunny Yu on 15/7/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Mark Six Reminder/Mark_Six_ReminderApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mark_Six_ReminderApp.swift
+//  Mark Six Reminder
+//
+//  Created by Sunny Yu on 15/7/2026.
+//
+
+import SwiftUI
+
+@main
+struct Mark_Six_ReminderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
